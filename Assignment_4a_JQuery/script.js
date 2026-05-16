54f11406bf2fd56d41caabe77f35047091810246
pekemonlover <pokemon54226@gmail.com>
2026-05-17 00:29:37 +0530

    Add files via upload

---
+++ Assignment_4a_JQuery/script.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+
+    // Red Button
+    $("#redBtn").click(function(){
+
+        $("body").css("background-color", "red");
+
+    });
+
+
+    // Blue Button
+    $("#blueBtn").click(function(){
+
+        $("body").css("background-color", "blue");
+
+    });
+
+
+    // Green Button
+    $("#greenBtn").click(function(){
+
+        $("body").css("background-color", "green");
+
+    });
+
+
+    // Yellow Button
+    $("#yellowBtn").click(function(){
+
+        $("body").css("background-color", "yellow");
+
+    });
+
+});
