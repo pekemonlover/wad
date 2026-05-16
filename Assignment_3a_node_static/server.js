92e7f6602f5d59aea7fa1b67acc12af0654d89c1
pekemonlover <pokemon54226@gmail.com>
2026-05-17 00:29:03 +0530

    Add files via upload

---
+++ Assignment_3a_node_static/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = 3000;
+
+// Serve static files from public folder
+app.use(express.static('public'));
+
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
